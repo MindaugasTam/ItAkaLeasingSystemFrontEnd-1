3120591dbd41509009aa45691ae10a29d2246969
CrazyRussian6 <35679680+CrazyRussian6@users.noreply.github.com>
2018-03-21 17:03:11 +0200

    Adding buttons

--- src/app/input-business-user-info/input-business-user-info.component.ts
+++ src/app/input-business-user-info/input-business-user-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-input-business-user-info',
@@ -7,8 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputBusinessUserInfoComponent implements OnInit {
 
-  constructor() { }
+  private businessUser: FormGroup;
 
+  constructor(fb: FormBuilder, private router: Router ) {
+    this.businessUser=fb.group({
+
+    });
+   }
+
+   toPreviousPage(){
+    console.log("Back");
+    this.router.navigate(['/input-loan-info']);
+  }
+
+
+  send(){
+    this.router.navigate(['/business-user-loan-report']);
+  }
   ngOnInit() {
   }
 
--- src/app/input-private-user-info/input-private-user-info.component.ts
+++ src/app/input-private-user-info/input-private-user-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-input-private-user-info',
@@ -7,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputPrivateUserInfoComponent implements OnInit {
 
-  constructor() { }
+public privateUser: FormGroup;
+
+  constructor(fb: FormBuilder, private router: Router) {
+    this.privateUser=fb.group({
+    })
+   }
+
+  toPreviousPage(){
+    console.log("Back");
+    this.router.navigate(['/input-loan-info']);
+  }
+
+  send(){
+    console.log("Send");
+    this.router.navigate(['/private-user-loan-report']);
+  }
+  
+  
+  
 
   ngOnInit() {
   }
