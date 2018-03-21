4bc29e1a297617f33e9801a3fad747d52a32acd4
unknown <matas.graj@gmail.com>
2018-03-21 16:26:45 +0200

    formated reacted forms for private and business users info pages

--- src/app/input-business-user-info/input-business-user-info.component.ts
+++ src/app/input-business-user-info/input-business-user-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { DataStoreService } from '../services/data-store.service';
 
 @Component({
   selector: 'app-input-business-user-info',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputBusinessUserInfoComponent implements OnInit {
 
-  constructor() { }
+  public businessUserInputForm: FormGroup;
+
+  constructor(fb: FormBuilder, private router: Router) {
+    this.businessUserInputForm = fb.group({
+      companyName:null,
+      companyCode:null,
+      email:null,
+      phoneNumber:null,
+      adress:null
+    })
+   }
+   send() {
+    console.log(this.businessUserInputForm.value);
+    this.router.navigate(['/input-business-user-info']);
+  }
+
+  reset(){
+    this.businessUserInputForm.reset();
+  }
 
   ngOnInit() {
+    let dataStore = new DataStoreService();
   }
 
 }
