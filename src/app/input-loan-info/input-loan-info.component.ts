be3f56060edc4893e93f994b2a08c1c806794f50
Karolis <kar.zukas@gmail.com>
2018-03-21 17:17:12 +0200

    validation done

--- src/app/input-loan-info/input-loan-info.component.ts
+++ src/app/input-loan-info/input-loan-info.component.ts
@@ -16,22 +16,41 @@ export class InputLoanInfoComponent implements OnInit {
 
     constructor(fb: FormBuilder, private router: Router ){
       this.loanForm = fb.group({
-        customerType:[null, Validators.required],
-        assetType:[null, Validators.required],
-        carBrand:[null, Validators.required],
-        carModel:[null, Validators.required],
-        enginePower:[0, Validators.required],
-        assetPrice:[null, Validators.required],
-        paymentPercentage:[null, Validators.required],
-        paymentAmount:[null, Validators.required],
+        customerType:['Private', Validators.required],
+        assetType:['Vehicle', Validators.required],
+        carBrand:['Not selected', Validators.required],
+        carModel:['Not selected', Validators.required],
+        year: [2000, [Validators.required, Validators.minLength(4), Validators.min(2000), Validators.maxLength(4), Validators.max(new Date().getFullYear())]],
+        enginePower:[1, [Validators.required, Validators.max(999), Validators.maxLength(3), Validators.min(1)]],
+        assetPrice:[0, Validators.required, Validators.min[0]],
+        paymentPercentage:[10, [Validators.required, Validators.min(10), Validators.max(100)]],
         leasePeriod:[null, Validators.required],
         margin:[null, Validators.required],
         contractFee:[null, Validators.required],
         paymentDay:[null, Validators.required]
       })
+
     }
 
+    calculateAdvancedPaymentAmount(){
+      return (this.assetPriceValue*(this.paymentPercentageValue/100))-this.assetPriceValue);
+    }
+
+    get assetType(){return this.loanForm.get('assetType') as FormControl};
     get customerType(){return this.loanForm.get('customerType') as FormControl};
+    get year(){return this.loanForm.get('year') as FormControl};
+    get paymentAmount(){return this.loanForm.get('paymentAmount') as FormControl};
+    get carBrand(){return this.loanForm.get('carBrand') as FormControl};
+    get advancedPaymentAmount(){return this.advancedPaymentAmount};
+    get enginePower(){return this.loanForm.get('enginePower') as FormControl};
+    get assetPrice(){return this.loanForm.get('assetPrice') as FormControl};
+    get paymentDay(){return this.loanForm.get('paymentDay') as FormControl};
+    get paymentPercentage(){return this.loanForm.get('paymentPercentage') as FormControl};
+
+
+    get assetPriceValue(){return this.loanForm.get('assetPrice').value}
+    get paymentPercentageValue(){return this.loanForm.get('paymentPercentage').value}
+
 
     send(){
       //validuojam, irasom i datastore service
