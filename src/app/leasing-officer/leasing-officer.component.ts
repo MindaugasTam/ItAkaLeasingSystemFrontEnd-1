5581eb083ffd4b3eca3c3ab38f85b3acbe8a0143
Lukas Janulis <lukas.janulis@gmail.com>
2018-04-09 22:24:02 +0300

    Officer menu styling

--- src/app/leasing-officer/leasing-officer.component.ts
+++ src/app/leasing-officer/leasing-officer.component.ts
@@ -86,7 +86,9 @@ export class LeasingOfficerComponent implements OnInit {
 
     this.vehicleService.updateVehicleLeasingStatus(leasingToUpdate.id, leasingToUpdate)
       .then(data => {
+        leasingToUpdate.statusChanged = true;
         this.selectedLeasing = leasingToUpdate;
+        //console.log(this.selectedLeasing);
       })
       .catch((error: any) => {
         console.log(error);
